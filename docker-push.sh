722144b3921a516823249c3ef38cb5001b982024
Kristoffer Josefsson <kejace@gmail.com>
2019-07-15 14:54:13 -0400

    Update docker-push.sh

--- docker-push.sh
+++ docker-push.sh
@@ -2,6 +2,6 @@
 set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 SHORTREF=`git rev-parse --short HEAD`
-docker push foamspace/plasma-mvp-sidechain:latest
-docker tag foamspace/plasma-mvp-sidechain:latest foamspace/plasma-mvp-sidechain:$SHORTREF
-docker push foamspace/plasma-mvp-sidechain:$SHORTREF
+docker push foamspace/plasma-demo:latest
+docker tag foamspace/plasma-demo:latest foamspace/plasma-demo:$SHORTREF
+docker push foamspace/plasma-demo:$SHORTREF
